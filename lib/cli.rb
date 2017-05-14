d331820597e896143c6b31e2a9116b95af5702cd
Dave Schlauderaff <dschlaud@gmail.com>
2017-05-14 15:44:38 -0500

    completed stub of cli menu interface

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,16 +15,33 @@ class MetacriticGames::CLI
       menu.index = :number
       menu.index_suffix = ")"
       menu.prompt = "Please choose the platform you want new release info for:"
-      menu.choice :PS4 do cli.say("List of new releases on PS4") end
-      menu.choice :"XBOX ONE" do cli.say("List of new releases on XBOX ONE") end
-      menu.choice :SWITCH do cli.say("List of new releases on SWITCH") end
-      menu.choice :PC do cli.say("List of new releases on PC") end
-      menu.choice :"WII U" do cli.say("List of new releases on WII U") end
-      menu.choice :"3DS" do cli.say("List of new releases on 3DS") end
-      menu.choice :"PS VITA" do cli.say("List of new releases on PS VITA") end
-      menu.choice :IOS do cli.say("List of new releases on IOS") end
+      menu.choice :PS4 do list_games("PS4") end
+      menu.choice :"Xbox One" do list_games("Xbox One") end
+      menu.choice :Switch do list_games("Switch") end
+      menu.choice :PC do list_games("PC") end
+      menu.choice :"Wii U" do list_games("Wii U") end
+      menu.choice :"3DS" do list_games("3DS") end
+      menu.choice :"PS Vita" do list_games("PS Vita") end
+      menu.choice :IOS do list_games("IOS") end
+      menu.choice :List do list_platforms end
       menu.choice :Exit do goodbye end
 
+    end
+  end
+
+  def list_games(platform)
+    cli.say "These are Metacritic's newest releases for #{platform}:"
+    self.cli.choose do |menu|
+      menu.index = :number
+      menu.index_suffix = ")"
+      menu.prompt = "Please choose the game you want more information on:"
+      menu.choice :game_1 do cli.say "Game 1 info" end
+      menu.choice :game_2 do cli.say "Game 2 info" end
+      menu.choice :game_3 do cli.say "Game 3 info" end
+      menu.choice :game_4 do cli.say "Game 4 info" end
+      menu.choice :game_5 do cli.say "Game 5 info" end
+      menu.choice :List do list_games(platform) end
+      menu.choice :Exit do goodbye end
     end
   end
 
