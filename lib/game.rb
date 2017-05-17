f51d1ca4f6c182031c55660c9a88cca4b114f3f5
Dave Schlauderaff <dschlaud@gmail.com>
2017-05-17 15:48:25 -0500

    added in more control statements for game #self.create_games

--- lib/game.rb
+++ lib/game.rb
@@ -40,25 +40,44 @@ class MetacriticGames::Game
   end
 
   def self.create_games(game_array)
+    binding.pry
     game_array.each do |game|
-      binding.pry
+      # binding.pry
       if game[:platform] == "XONE"               #metacritic naming for the xboxone does not follow standard pattern
-        platform = MetacriticGames::Platform.all.select {|platform| platform.name == "Xbox One"}
+        platform = MetacriticGames::Platform.all.find {|platform| platform.name == "Xbox One"}
 
         game.tap do |new_game|
           new_game = self.find_or_create_by_name(game[:name])
           # binding.pry
-          new_game.add_platform(platform.select {|i| i.name == game[:platform]})
+          new_game.add_platform(platform)
+          new_game.url[:"#{game[:platform]}"] = game[:url]
+        end
+      elsif game[:platform] == "WIIU"               #metacritic naming for the wii u does not follow standard pattern
+        platform = MetacriticGames::Platform.all.find {|platform| platform.name == "Wii U"}
+
+        game.tap do |new_game|
+          new_game = self.find_or_create_by_name(game[:name])
+          # binding.pry
+          new_game.add_platform(platform)
+          new_game.url[:"#{game[:platform]}"] = game[:url]
+        end
+      if game[:platform] == "VITA"               #metacritic naming for the ps vita does not follow standard pattern
+        platform = MetacriticGames::Platform.all.find {|platform| platform.name == "PS Vita"}
+
+        game.tap do |new_game|
+          new_game = self.find_or_create_by_name(game[:name])
+          # binding.pry
+          new_game.add_platform(platform)
           new_game.url[:"#{game[:platform]}"] = game[:url]
 
         end
       else
-        platform = MetacriticGames::Platform.all.select {|platform| platform.name == game[:platform]}
+        platform = MetacriticGames::Platform.all.find {|platform| platform.name == game[:platform]}
 
         game.tap do |new_game|
           new_game = self.find_or_create_by_name(game[:name])
-          binding.pry
-          new_game.add_platform(platform.select {|i| i.name == game[:platform]})
+          # binding.pry
+          new_game.add_platform(platform)
           new_game.url[:"#{game[:platform]}"] = game[:url]
         end
       end
